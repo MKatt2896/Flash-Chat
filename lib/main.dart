db50d8b7152c33fb0e4bbdb683d73e90b80a23bd
MahDooD <mahmoud.khattab2896@gmail.com>
2022-01-02 17:03:31 +0200

    first Flash Chat commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,34 @@ import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(FlashChat());
 
-class FlashChat extends StatelessWidget {
+class FlashChat extends StatefulWidget {
+  @override
+  State<FlashChat> createState() => _FlashChatState();
+}
+
+class _FlashChatState extends State<FlashChat> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
     );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Firebase.initializeApp().whenComplete(() => print('completed'));
+    setState(() {});
   }
 }
